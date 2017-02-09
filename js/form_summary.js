254a804e8fc4343fa06a4afd346802eec7565702
Magnus Li <li.magnus@gmail.com>
2017-02-09 16:00:24 +0100

    Added offline font OpenSans

--- js/form_summary.js
+++ js/form_summary.js
@@ -85,10 +85,10 @@ function generateCompleteButton() {
 		showConfirmationBox("Are you sure you want to the complete the form?", function() {
 			setToCompleted(form);
 			LS.updateFacility(facility);
-			
 			//if allowed approval: refresh and prompt for instant approval after completion
 			if (allowedApproval("TEMP")) {
 				navigateToAddress("form_summary.html#facility=" + facilityId + "#cycle=" + cycleId + "#form=" + formId + "#promptForInstantApproval=true");
+				location.reload();
 			} else {
 				location.reload();
 			}
